3b4fdb24ab5d41f21f5c9fa55a98f3d7e478bb44
himaloyash <himaloyashis98@gmail.com>
2023-03-23 13:34:25 +0600

    another small project on starting react

---
+++ src/App.js
@@ -0,0 +1,47 @@
+
+
+import { useState } from 'react';
+import './App.css';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Phone></Phone>
+      <District name='kopila' age='teen' profession='actresss'></District>
+      <District name='Urvashi' age='teen' profession='actresss'></District>
+      <District name='katrina' age='teen' profession='actresss'></District>
+     
+    </div>
+  );
+}
+
+
+function Phone(){
+  const [Phones, setPhones] = useState(100)
+
+  const Charger=()=>{
+    const PhoneCharge = Phones - 10;
+    setPhones(PhoneCharge)
+  }
+  return(
+    <div>
+      <h1>charge: {Phones} </h1>
+        <button onClick={Charger}>Charge Percent</button>
+    </div>
+  )
+}
+
+
+function District(props){
+  return(
+    <div className='district'>
+      <h2>keMon aco oo bokuli?</h2>
+      <h3>name: {props.name}  </h3>
+      <p>age :{props.age}</p>
+      <p>profession : {props.profession}</p>
+    </div>
+  )
+}
+
+export default App;
